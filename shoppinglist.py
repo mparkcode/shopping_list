55ab48281b7c91b424d6144e4fdaaf4272ded7ef
Michael Park <mparkcode@gmail.com>
2018-05-30 14:05:57 +0000

    username login routes add

--- shoppinglist.py
+++ shoppinglist.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
-items = []
-password = []
+user_items = {'mike':[]}
+users = {'mike':'mike'}
 
 @app.route("/")
 def login():
@@ -13,18 +13,34 @@ def login():
 def do_login():
     username = request.form['username']
     passw = request.form['password']
-    password.append(passw)
-    print(password)
+    if username not in users:
+        return render_template("no_user.html", username=username)
+    elif username in users:
+        if users[username] != passw:
+            return render_template("incorrect_password.html", username=username)
+    return redirect("/" + username + "/list")
+    
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    username = request.form['username']
+    passw = request.form['password']
+    check_passw = request.form['check_password']
+    if passw != check_passw:
+        return render_template("signup_fail.html", username=username)
+    users[username] = passw
+    user_items[username]=[]
+    print(users)
+    print(user_items)
     return redirect("/" + username + "/list")
 
 @app.route("/<username>/list")
 def get_index(username):
     # return password[0]
     total = 0
-    for i in items:
+    for i in user_items[username]:
         total += i['total']
         total = round(total,2)
-    return render_template("shoppinglist.html", items = items, total=total, username=username)
+    return render_template("shoppinglist.html", user_items = user_items, total=total, username=username)
     
 @app.route("/<username>/add_item", methods=["POST","GET"])
 def add_item(username):
@@ -37,20 +53,20 @@ def add_item(username):
         'price':price,
         'total': round(quantity_float * price, 2)
     }
-    items.append(item)
+    user_items[username].append(item)
     return redirect("/"+username+"/list")
     
 @app.route("/<username>/<item>/remove", methods=["POST","GET"])
 def remove_item(username,item):
-    for i in items:
+    for i in user_items[username]:
         if i['item'] == item:
-            items.remove(i)
+            user_items[username].remove(i)
     return redirect("/"+username+"/list")
     
 @app.route("/<username>/<item>/change", methods=["POST", "GET"])
 def change(username,item):
     item = item
-    for i in items:
+    for i in user_items[username]:
         if i['item'] == item:
             item = i
     return render_template("change.html", item=item, username=username)
@@ -61,7 +77,7 @@ def change_quantity(username,item):
     new_quantity = request.form["new_quantity"]
     new_quantity_float = float(new_quantity)
     new_price = float(request.form["new_price"])
-    for i in items:
+    for i in user_items[username]:
         if i['item'] == item:
             i['quantity'] = new_quantity
             i['price'] = new_price
